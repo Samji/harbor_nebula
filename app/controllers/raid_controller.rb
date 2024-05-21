7b124b34919f02ab9bba4645a846060bcc62b400
Sam <Samuel3877@hotmail.co.uk>
2024-05-21 13:48:25 +0100

    Got the faction form working

--- app/controllers/raid_controller.rb
+++ app/controllers/raid_controller.rb
@@ -2,12 +2,6 @@ class RaidController < ApplicationController
   
   def index
     @factions = Faction.all
-    @rarities = Rarity.all
-    @roles = Role.all
-    @affinities = Affinity.all
-  end
-  
-  def new
     @faction = Faction.new
   end
 
@@ -18,7 +12,6 @@ class RaidController < ApplicationController
       redirect_to raid_path, notice: 'Faction was successfully created.'
     else
       @factions = Faction.all
-      logger.debug @faction.errors.full_messages.to_sentence
       render :index
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/raid', to: 'raid#index'
-  post 'raid', to: 'raid#index'
+  post '/raid', to: 'raid#create'
 end
 
