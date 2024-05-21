67dcb34365ae9b485f4257707d9d124b47bf8ae4
Sam <Samuel3877@hotmail.co.uk>
2024-05-21 19:53:45 +0100

    Not working properly
    I've added rarity and role
    When I submit a new anything, I cannot submit any more anythings until I manually refresh the page

--- app/controllers/raid_controller.rb
+++ app/controllers/raid_controller.rb
@@ -6,15 +6,32 @@ class RaidController < ApplicationController
 
     @rarities = Rarity.all
     @rarity = Rarity.new
+    
+    @affinities = Affinity.all
+    @affinity = Affinity.new
+
+    @roles = Role.all
+    @role = Role.new
   end
 
   def create_faction
     @faction = Faction.new(faction_params)
     if @faction.save
       @factions = Faction.all
+      @faction = Faction.new
+      @rarities = Rarity.all
+      @rarity = Rarity.new
+      @affinities = Affinity.all
+      @affinity = Affinity.new
+      @roles = Role.all
+      @role = Role.new
       redirect_to raid_path, notice: 'Faction was successfully created.'
     else
       @factions = Faction.all
+      @faction = Faction.new
+      @rarities = Rarity.all
+      @affinities = Affinity.all
+      @roles = Role.all
       render :index
     end
   end
@@ -22,10 +39,41 @@ class RaidController < ApplicationController
   def create_rarity
     @rarity = Rarity.new(rarity_params)
     if @rarity.save
-      @rarities = Rarity.all
       redirect_to raid_path, notice: 'Rarity was successfully created.'
     else
+      @factions = Faction.all
       @rarities = Rarity.all
+      @rarity = Rarity.new
+      @affinities = Affinity.all
+      @roles = Role.all
+      render :index
+    end
+  end
+
+  def create_affinity
+    @affinity = Affinity.new(affinity_params)
+    if @affinity.save
+      redirect_to raid_path, notice: 'Affinity was successfully created.'
+    else
+      @factions = Faction.all
+      @rarities = Rarity.all
+      @affinities = Affinity.all
+      @affinity = Affinity.new
+      @roles = Role.all
+      render :index
+    end
+  end
+
+  def create_role
+    @role = Role.new(role_params)
+    if @role.save
+      redirect_to raid_path, notice: 'Role was successfully created.'
+    else
+      @factions = Faction.all
+      @rarities = Rarity.all
+      @affinities = Affinity.all
+      @roles = Role.all
+      @role = Role.new
       render :index
     end
   end
@@ -38,6 +86,14 @@ class RaidController < ApplicationController
 
     def rarity_params
       params.require(:rarity).permit(:name)
+    end
+
+    def affinity_params
+      params.require(:affinity).permit(:name)
+    end
+
+    def role_params
+      params.require(:role).permit(:name)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/raid', to: 'raid#index'
   post '/raid/create_faction', to: 'raid#create_faction', as: 'create_faction'
   post '/raid/create_rarity', to: 'raid#create_rarity', as: 'create_rarity'
+  post '/raid/create_affinity', to: 'raid#create_affinity', as: 'create_affinity'
+  post '/raid/create_role', to: 'raid#create_role', as: 'create_role'
 end
 
