f6f887a309d44609fa66c2dfa9837781820234f1
Sam <Samuel3877@hotmail.co.uk>
2024-05-21 15:15:45 +0100

    Successful raid index
    Factions and Rarities visible in a table
    Forms to create both a faction or a rarity are working
    Updated the routes to add two raid actions for creating factions and rarities named as create_faction_path and create_rarity_path

--- app/controllers/raid_controller.rb
+++ app/controllers/raid_controller.rb
@@ -3,9 +3,12 @@ class RaidController < ApplicationController
   def index
     @factions = Faction.all
     @faction = Faction.new
+
+    @rarities = Rarity.all
+    @rarity = Rarity.new
   end
 
-  def create
+  def create_faction
     @faction = Faction.new(faction_params)
     if @faction.save
       @factions = Faction.all
@@ -15,11 +18,26 @@ class RaidController < ApplicationController
       render :index
     end
   end
+
+  def create_rarity
+    @rarity = Rarity.new(rarity_params)
+    if @rarity.save
+      @rarities = Rarity.all
+      redirect_to raid_path, notice: 'Rarity was successfully created.'
+    else
+      @rarities = Rarity.all
+      render :index
+    end
+  end
   
   private
 
     def faction_params
       params.require(:faction).permit(:name)
+    end
+
+    def rarity_params
+      params.require(:rarity).permit(:name)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/raid', to: 'raid#index'
-  post '/raid', to: 'raid#create'
+  post '/raid/create_faction', to: 'raid#create_faction', as: 'create_faction'
+  post '/raid/create_rarity', to: 'raid#create_rarity', as: 'create_rarity'
 end
 
